2e0a77beb019f70345912d691d2f0b6c701f9df2
Ken <84665430+KennKane@users.noreply.github.com>
2022-04-03 22:53:16 +0545

    Added two structures (ball and paddle)

--- src/Main.c
+++ src/Main.c
@@ -1,21 +1,34 @@
 #include<stdio.h>
 #include<SDL.h>
-#define SCREEN_WIDTH 940
-#define SCREEN_HEIGHT 680
+#define SCREEN_WIDTH 940    //window width
+#define SCREEN_HEIGHT 680   //window height
+
+struct ball{
+    int x, y;   //position of the ball
+    int w, h;   //width and height of the ball
+}ball;
+
+struct paddle{
+    int x, y;   //position of the paddle
+    int w, h;   //width and height of the paddle
+}paddle_1, paddle_2;
 
 int main(int argc, char *args[]){
-    SDL_Window *window = NULL; //declaring pointer
-    SDL_Surface *screenSurface = NULL; //declaring pointer
+    SDL_Window *window = NULL; //SDL_Window is a structure, declaring window pointer
+    
+    
+    SDL_Surface *screenSurface = NULL; //SDL_Surface is a structure, declaring screenSurface pointer
     
     SDL_Init(SDL_INIT_VIDEO); //Initalization of SDL2
-    
+
+    //Creating a window with following settings   
     window = SDL_CreateWindow( 
-        "Pong",
-        SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
-        SCREEN_WIDTH, 
-        SCREEN_HEIGHT, 
-        SDL_WINDOW_SHOWN 
+        "Pong",                     //Window title
+        SDL_WINDOWPOS_UNDEFINED,    //initial x position
+        SDL_WINDOWPOS_UNDEFINED,    //initial y position
+        SCREEN_WIDTH,               //width, in pixels
+        SCREEN_HEIGHT,              //height, in pixels 
+        0 
     );
     //Checks if window was successfully created
     if(window == NULL){
@@ -36,7 +49,7 @@ int main(int argc, char *args[]){
     }
     
     //Destroy window
-    SDL_DestroyWindow( window );
+    SDL_DestroyWindow(window);
 
     //Quit SDL subsystems
     SDL_Quit();
